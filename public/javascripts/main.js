797f9af910b188b0a06e878511fd84471d49ca27
sk0rp <asawoniewicz@gmail.com>
2013-09-11 17:14:46 +0200

    js for polledit

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,22 +1,37 @@
 $(document).ready(function(){
-    var i = $('#questions input.questionInput').length;
+	var getQuestionId = function(oDiv) {
+		return parseInt(oDiv.find('.questionCount').val(), 10);
+	};
+
+	
+	var incrementId = function(oDiv) {
+		var newId = getQuestionId(oDiv) + 1;
+		
+		oDiv.find('input:text').each(function() {
+			$(this).prop('name', $(this).prop('name').replace(/([\d])+/, newId));
+		});
+		oDiv.find('.questionCount').val(newId);
+	};
 	
 	var addAnswer = function() {
 		var count = $(this).parent().parent().find('.questionCount').val();
 		$('<input type="text" name="poll.question[' + count + '].answer[]" style="display: block" />').fadeIn('slow').appendTo($(this).parent().parent().find('.answers'));
-	}
+	};
     $('.addAnswer').click(addAnswer);
 	
 	var addQuestion = function() {
+		var prev = $(this).parent().parent();
+		var id = getQuestionId(prev);
+		alert(id);
         var divQuestion = $('<div class="question" />');
-		divQuestion.append($('<input type="text" name="poll.question[' + i + '].question" class="questionInput" />'));
-		divQuestion.append($('<input type="hidden" name="questionCount" value="' + i + '" class="questionCount" />'));
+		divQuestion.append($('<input type="text" name="poll.question[' + id + '].question" class="questionInput" />'));
+		divQuestion.append($('<input type="hidden" name="questionCount" value="' + id + '" class="questionCount" />'));
 		var divAnswersCont = $('<div class="answersCont" />');
 		var divField = $('<div class="field">');
-		divField.append($('<input type="checkbox" name="poll.question[' + i + '].multiple" id="poll.question[' + i + '].multiple" class="checkbox" />'));
-		divField.append($('<label for="poll.question[' + i + '].multiple" class="label_checkbox">Allow Multiple Answer Selection</label>'));
+		divField.append($('<input type="checkbox" name="poll.question[' + id + '].multiple" id="poll.question[' + id + '].multiple" class="checkbox" />'));
+		divField.append($('<label for="poll.question[' + id + '].multiple" class="label_checkbox">Allow Multiple Answer Selection</label>'));
 		var divAnswers = $('<div class="answers" />');
-		divAnswers.append($('<input type="text" name="poll.question[' + i + '].answer[]" />'));
+		divAnswers.append($('<input type="text" name="poll.question[' + id + '].answer[]" />'));
 		divAnswersCont.append(divField);
 		divAnswersCont.append(divAnswers);
 		divQuestion.append(divAnswersCont);
@@ -26,14 +41,16 @@ $(document).ready(function(){
 		divQuestionsButtons.append(buttonAddAnswer);
 		divQuestionsButtons.append(buttonAddQuestion);
 		divQuestion.append(divQuestionsButtons);
-		($(this).parent().parent()).after(divQuestion.fadeIn('slow'));
-        i++;
-    }
+		(prev).after(divQuestion.fadeIn('slow'));
+		prev.nextAll().each(function() {
+			incrementId($(this));
+		});
+    };
 	$('.addQuestion').click(addQuestion);
 
 	var delQuestion = function() {
 		$(this).parent().parent().remove();
-	}
+	};
     $('.delQuestion').click(delQuestion);
  
 });
